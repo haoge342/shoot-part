65dd7c678dddf4bc2431f802273544c539accc4a
haoge342 <haoge342@gmail.com>
2025-03-01 17:23:00 -0500

    cmake

---
+++ build/CMakeFiles/shoot-part.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shoot-part.dir/main.cpp.o"
+  "CMakeFiles/shoot-part.dir/main.cpp.o.d"
+  "CMakeFiles/shoot-part.dir/particle.cpp.o"
+  "CMakeFiles/shoot-part.dir/particle.cpp.o.d"
+  "shoot-part"
+  "shoot-part.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shoot-part.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/shoot-part.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for shoot-part.
+# This may be replaced when dependencies are built.
